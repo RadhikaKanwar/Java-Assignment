3ee4e34079004b8a8c8d2ac550e61d0eef30fac7
radhika-kanwar <radhikakanwar0806@gmail.com>
2023-11-19 20:29:16 +0530

    Committing after adding the switch statement

--- PS1Task.java
+++ PS1Task.java
@@ -4,6 +4,52 @@ public class PS1Task {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int choice;
+        do { 
+ 
+            System.out.println("\nSelect the program you want to execute: ");
+            System.out.println("1. Input Book Details");
+            System.out.println("2. Check Availability and Issue Books");
+            System.out.println("3. Return Book");
+            System.out.println("4. Temperature Conversion in Library HVAC System");
+            System.out.println("5. Exit");
+
+            choice = sc.nextInt();
+
+            switch(choice) {
+                case 1:
+                {
+
+                }
+                break;
+            
+                case 2:
+                {
+
+                }
+                break;
+            
+                case 3:
+                {
+
+                }
+                break;
+            
+                case 4:
+                {
+
+                }
+                break;
+            
+                case 5:
+                break;
+            
+                default:
+                System.out.println("Invalid choice! Enter a valid option.");
+                break;
+            }
+        } while (choice != 5);
+
         sc.close();
     }
 }
