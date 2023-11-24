8bd95610e831082ff675447ecc4fda5d209b6a3e
radhika-kanwar <radhikakanwar0806@gmail.com>
2023-11-24 23:35:08 +0530

    Last pattern in my brain

--- PS2Task.java
+++ PS2Task.java
@@ -3,23 +3,80 @@ import java.util.*;
 public class PS2Task {
 
     public static void IncrementalNumberSequence(Scanner sc) {
-
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println();
+        for(int i=1;i<=rows;i++) {
+            int counter = 1;
+            for (int j=1;j<=i;j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
     }
 
     public static void RepeatedNumberPattern(Scanner sc)  {
-
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println();
+        for(int i=1;i<=rows;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void StarDiamond(Scanner sc) {
+        System.out.print("Enter the number of rows in the diamond: ");
+        int rows = sc.nextInt();
+        System.out.println();
+        int num = (rows+1)/2;
+        
+        // upper triangle
+        for(int i=1;i<=num;i++){
+            // leading spaces
+            for(int k=num-i;k>0;k--) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("* " );
+            }
+            System.out.println();
+        }
 
+        // bottom triangle
+        for(int i=num-1;i>=1;i--){
+            // leading spaces
+            for(int k=num-i;k>0;k--) {
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--) {
+                System.out.print("* " );
+            }
+            System.out.println();
+        }
     }
 
     public static void NumberPyramid(Scanner sc) {
-
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println();
+        int counter = 1;
+        for(int i=1;i<=rows;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
     }
 
     public static void MixedNumberPattern(Scanner sc) {
-
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println();
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -33,35 +90,40 @@ public class PS2Task {
             System.out.println("3. Create a Star Diamond");
             System.out.println("4. Construct a Number Pyramid");
             System.out.println("5. Create a Mixed Number Pattern");
-            System.out.println("6. Exit");
+            System.out.println("6. Exit\n");
 
+            System.out.print("Choice: ");
             choice = sc.nextInt();
 
             switch(choice) {
                 case 1:
-                break;
+                    IncrementalNumberSequence(sc);    
+                    break;
             
                 case 2:
-                break;
+                    RepeatedNumberPattern(sc);
+                    break;
             
                 case 3:
-                
-                break;
+                    StarDiamond(sc);
+                    break;
             
                 case 4:
-                break;
+                    NumberPyramid(sc);
+                    break;
             
                 case 5:
-                break;
+                    MixedNumberPattern(sc);
+                    break;
 
                 case 6:
-                System.out.println("Adios!");
-                sc.close();
-                System.exit(0);
+                    System.out.println("Adios!");
+                    sc.close();
+                    System.exit(0);
             
                 default:
-                System.out.println("Invalid choice! Enter a valid option.");
-                break;
+                    System.out.println("Invalid choice! Enter a valid option.");
+                    break;
             }
         } while (choice != 6);
     }
---
+++ PS5Task.java
@@ -0,0 +1,5 @@
+public class PS5Task {
+    public static void main(String[] args) {
+        
+    }
+}
