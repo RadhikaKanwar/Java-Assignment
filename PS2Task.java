a8418f40e467e217f460e5a4afd739c956ae65bf
radhika-kanwar <radhikakanwar0806@gmail.com>
2023-11-24 11:35:51 +0530

    Structure completed

--- PS2Task.java
+++ PS2Task.java
@@ -1,6 +1,26 @@
 import java.util.*;
 
 public class PS2Task {
+
+    public static void IncrementalNumberSequence(Scanner sc) {
+
+    }
+
+    public static void RepeatedNumberPattern(Scanner sc)  {
+
+    }
+
+    public static void StarDiamond(Scanner sc) {
+
+    }
+
+    public static void NumberPyramid(Scanner sc) {
+
+    }
+
+    public static void MixedNumberPattern(Scanner sc) {
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,48 +28,41 @@ public class PS2Task {
         do { 
  
             System.out.println("\nSelect the program you want to execute: ");
-            System.out.println("1. Input Book Details");
-            System.out.println("2. Check Availability and Issue Books");
-            System.out.println("3. Return Book");
-            System.out.println("4. Temperature Conversion in Library HVAC System");
-            System.out.println("5. Exit");
+            System.out.println("1. Generate Incremental Number Sequence");
+            System.out.println("2. Visuslize Repeated Number Pattern");
+            System.out.println("3. Create a Star Diamond");
+            System.out.println("4. Construct a Number Pyramid");
+            System.out.println("5. Create a Mixed Number Pattern");
+            System.out.println("6. Exit");
 
             choice = sc.nextInt();
 
             switch(choice) {
                 case 1:
-                {
-
-                }
                 break;
             
                 case 2:
-                {
-
-                }
                 break;
             
                 case 3:
-                {
-
-                }
+                
                 break;
             
                 case 4:
-                {
-
-                }
                 break;
             
                 case 5:
                 break;
+
+                case 6:
+                System.out.println("Adios!");
+                sc.close();
+                System.exit(0);
             
                 default:
                 System.out.println("Invalid choice! Enter a valid option.");
                 break;
             }
-        } while (choice != 5);
-
-        sc.close();
+        } while (choice != 6);
     }
 }
