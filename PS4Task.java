e1cfbfa7e1a91621e7747927eb00f4d1d1b3cdbb
radhika-kanwar <radhikakanwar0806@gmail.com>
2023-12-08 19:40:55 +0530

    Base ready

--- PS4Task.java
+++ PS4Task.java
@@ -1,6 +1,26 @@
 import java.util.*;
 
 public class PS4Task {
+
+    public static void PrimeNumberDetector(Scanner sc) {
+
+    }
+
+    public static void QuantumSimilarity(Scanner sc) {
+
+    }
+
+    public static void ZeroSequence(Scanner sc) {
+
+    }
+
+    public static void ArrayAnalyzer(Scanner sc) {
+
+    }
+
+    public static void  SortingQuantum(Scanner sc) {
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -8,47 +28,47 @@ public class PS4Task {
         do { 
  
             System.out.println("\nSelect the program you want to execute: ");
-            System.out.println("1. Input Book Details");
-            System.out.println("2. Check Availability and Issue Books");
-            System.out.println("3. Return Book");
-            System.out.println("4. Temperature Conversion in Library HVAC System");
-            System.out.println("5. Exit");
+            System.out.println("1. Prime Number Detector");
+            System.out.println("2. Quantum Similarity");
+            System.out.println("3. Zero Sequence in Binary");
+            System.out.println("4. Array Analyzer");
+            System.out.println("5. Sorting Quantum Data");
+            System.out.println("6. Exit\n");
 
+            System.out.print("Choice: ");
             choice = sc.nextInt();
 
             switch(choice) {
                 case 1:
-                {
-
-                }
-                break;
+                    PrimeNumberDetector(sc);
+                    break;
             
                 case 2:
-                {
-
-                }
-                break;
+                    QuantumSimilarity(sc);
+                    break;
             
                 case 3:
-                {
-
-                }
-                break;
+                    ZeroSequence(sc);
+                    break;
             
                 case 4:
-                {
-
-                }
-                break;
+                    ArrayAnalyzer(sc);
+                    break;
             
                 case 5:
-                break;
+                    SortingQuantum(sc);
+                    break;
+
+                case 6:
+                    System.out.println("Adios!");
+                    sc.close();
+                    System.exit(0);
             
                 default:
-                System.out.println("Invalid choice! Enter a valid option.");
-                break;
+                    System.out.println("Invalid choice! Enter a valid option.");
+                    break;
             }
-        } while (choice != 5);
+        } while (choice != 6);
 
         sc.close();
     }
--- PS5Task.java
+++ PS5Task.java
@@ -1,5 +1,74 @@
+import java.util.Scanner;
+
 public class PS5Task {
+    public static void TreasureMap(Scanner sc) {
+
+    }
+
+    public static void LostScrolls(Scanner sc) {
+
+    }
+
+    public static void EnigmaPatterns(Scanner sc) {
+
+    }
+
+    public static void SecondEnigma(Scanner sc) {
+
+    }
+
+    public static void CipherCracker(Scanner sc) {
+
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int choice;
+        do { 
+ 
+            System.out.println("\nSelect the program you want to execute: ");
+            System.out.println("1. The Treasure Map");
+            System.out.println("2. The Lost Scrolls");
+            System.out.println("3. The Enigma Pattern");
+            System.out.println("4. The Second Enigma");
+            System.out.println("5. The Cipher Cracker");
+            System.out.println("6. Exit\n");
+
+            System.out.print("Choice: ");
+            choice = sc.nextInt();
+
+            switch(choice) {
+                case 1:
+                    TreasureMap(sc);
+                    break;
+            
+                case 2:
+                    LostScrolls(sc);
+                    break;
+            
+                case 3:
+                    EnigmaPatterns(sc);
+                    break;
+            
+                case 4:
+                    SecondEnigma(sc);;
+                    break;
+            
+                case 5:
+                    CipherCracker(sc);
+                    break;
+
+                case 6:
+                    System.out.println("Adios!");
+                    sc.close();
+                    System.exit(0);
+            
+                default:
+                    System.out.println("Invalid choice! Enter a valid option.");
+                    break;
+            }
+        } while (choice != 6);
+
+        sc.close();
     }
 }
