59a07fd8ddc540ad2f503f4bcf599fd13aed51fc
radhika-kanwar <radhikakanwar0806@gmail.com>
2023-12-11 00:02:36 +0530

    Problem Sets

---
+++ ProblemSet.java
@@ -0,0 +1,170 @@
+public class ProblemSet {}
+
+/*
+Problem Set 0 - 
+
+Background:
+Imagine a local library that wants to automate its book management system. The librarian needs to perform operations like issue books, return books, check availability, and maintain records.
+
+Task:
+Program 1: Greet the Librarian
+Print a welcome message for the librarian.
+Example: "Welcome to ABC Library Management System!"
+
+Program 2: Input Book Details
+Ask the user to input details for a book: ISBN, Title, Author, Price.
+Store these details in appropriate data types.
+Display the entered details.
+
+Program 3: Check Availability and Issue Book
+Ask for the ISBN of the book to be issued.
+Check if the book with that ISBN is available.
+If available, issue the book and update availability status.
+If not available, display an appropriate message.
+
+Program 4: Return Book
+Ask for the ISBN of the book being returned.
+Update the availability status of the book.
+Calculate and display any late fees, if applicable.
+
+Program 5: Temperature Conversion in Library HVAC System
+Input the current temperature in Fahrenheit.
+Convert it to Celsius and display it.
+Suggest HVAC settings based on the converted temperature.
+     
+
+Problem Set 1 -
+
+Background:
+Imagine you are part of a space exploration team that's planning a mission to a distant planet. Your team needs a toolkit for various calculations and checks that are essential for the mission's success.
+
+Task:
+Program 1: Calculate Escape Velocity
+Given the radius and mass of a planet, calculate its escape velocity.
+Use the formula for escape velocity: √((2×G×M)/R)
+
+Program 2: Oxygen Availability Check
+Take the atmospheric pressure and oxygen percentage as input.
+Calculate the partial pressure of oxygen and determine if it's sufficient for humans.
+
+Program 3: Thruster Check
+Input the current speed and desired speed.
+Determine whether the thrusters should be activated to speed up, slow down, or maintain current speed.
+
+Program 4: Calculate Orbital Period
+Given the semi-major axis of an orbit, calculate the orbital period.
+Use the formula T=2×π×√((a^3)/(G×(M1+M2)))
+
+Program 5: Star Map
+Input the number of celestial bodies you want to map.
+Display a star map with that many points, where each point represents a celestial body.
+
+
+Problem Set 2 -
+
+Background:
+Imagine you're part of a team that creates educational software for students interested in geometry and mathematics. Your task is to develop a toolkit that will help visualize different types of patterns and shapes.
+
+Task:
+Program 1: Generate Incremental Number Sequences
+Given a number n, generate an incremental sequence up to n rows.
+Display each sequence in a separate row.
+
+Program 2: Visualize Repeated Number Patterns
+For each row i, print the number i repeatedly i times.
+Display the pattern up to n rows.
+
+Program 3: Create a Star Diamond
+Given the number of rows n, create a star diamond pattern.
+Use nested loops and conditional statements to accomplish this.
+
+Program 4: Construct a Number Pyramid
+Create a pyramid pattern using numbers.
+The topmost row should start with 1 and increase as we move down the rows.
+
+Program 5: Create a Mixed Number Pattern
+Design a pattern that uses a mix of incremental and decremental numbers.
+Use your imagination and mathematical skills to create an interesting pattern.
+
+
+Problem Set 3 -
+
+Background:
+Imagine you are a software developer for an international spy agency. Your mission, should you choose to accept it, involves decoding encrypted messages, converting units for global operations, and identifying numerical patterns that could be keys to unlock secret doors. The clock is ticking!
+
+Task:
+Program 1: Decipher the '@' and '*' Pattern
+Given an '@' and '*' pattern, reverse it.
+Add a feature to modify the pattern dynamically.
+
+Program 2: Decode Colored Blocks
+Analyze a pattern of colored blocks represented by different characters.
+Convert them into their respective color names.
+
+Program 3: Reverse Engineer Palindromes
+Reverse a number and check if it is a palindrome.
+If it's a palindrome, declare it as a "Key Code".
+
+Program 4: Double Agent Checker
+Examine a double value and determine if it's actually an integer in disguise.
+If it is, flag it as "Safe for Operation".
+
+Program 5: Global Unit Converter
+Convert kilometers to miles and vice versa.
+Display the converted value and declare if it's "Operationally Viable" or not.
+
+
+Problem Set 4 -
+
+Background:
+You've been hired by QuantumCorp, a front for a top-secret scientific research lab. Your task is to assist in solving complex problems using Java programs that range from finding prime numbers in a given range, to analyzing arrays, to even delving into bitwise operations!
+
+Task:
+Program 1: Prime Number Detector
+Find the prime numbers in a given range.
+Determine if the range contains "Quantum Prime Numbers" (Prime numbers that are one less than a power of 2).
+
+Program 2: Quantum Similarity
+Check if at least two out of three input values are similar.
+If similar, assess whether they can be considered "Quantum Entangled Pairs".
+
+Program 3: Zero Sequence in Binary
+Find the longest sequence of zeros in the binary representation of a number.
+Determine if the sequence length makes the number a "Quantum Stable" or "Quantum Unstable" number.
+
+Program 4: Array Analyzer
+Find the average of an array.
+Determine if the array contains "Quantum Elements" (Average is an integer and each element is evenly divisible by the average).
+
+Program 5: Sorting Quantum Data
+Perform sorting on numerical and string arrays.
+Evaluate if the sorted arrays can be used for "Quantum Encryption".
+
+
+Problem Set 5 -
+
+Background:
+You are working for an organization known only as "The Order," a group of highly skilled programmers and data analysts. Their mission: to solve the world's most perplexing problems using the power of code. You are their new recruit. Welcome to the Crypto Hunter Chronicles!
+
+Task:
+Program 1: The Treasure Map
+Search for a specific number in an array.
+If found, is the index a prime number? A prime index could point to a hidden treasure.
+
+Program 2: The Lost Scrolls
+Merge two arrays and sort them in decreasing order.
+Does the sorted array contain any rare 'gem' numbers? (E.g., numbers that are palindromes)
+
+Program 3: The Enigma Patterns
+Use a jagged array to print a unique pattern.
+Could the pattern be a secret code? Try to decipher it.
+
+Program 4: The Second Enigma
+Print another unique pattern using a jagged array.
+Compare it with the first pattern. Are there any similarities?
+
+Program 5: The Cipher Cracker
+Input a number and find the sum of its last N digits and the product of its first M digits.
+Use the sum and product to crack a fictional cipher.
+
+*/
